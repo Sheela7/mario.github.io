9a006f67e60fc0bfa2854283dda05bdbbdd2b23f
sheela7 <sheela7>
2023-06-07 23:15:03 +0545

    bugs fixed- music.js

--- javascript/loaders/music.js
+++ javascript/loaders/music.js
@@ -2,7 +2,7 @@ import { loadJSON } from "../loaders.js";
 import MusicPlayer from "../musicPlayer.js";
 
 export function loadMusicSheet(name) {
-  return loadJSON(`../music/${name}.json`).then((musicSheet) => {
+  return loadJSON(`./music/${name}.json`).then((musicSheet) => {
     const musicPlayer = new MusicPlayer();
     for (const [name, track] of Object.entries(musicSheet)) {
       musicPlayer.addTrack(name, track.url);
